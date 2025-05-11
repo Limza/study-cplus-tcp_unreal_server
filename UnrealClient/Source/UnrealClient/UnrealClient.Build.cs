8748c55435b9011a1f13a957da2674979324b303
jun <rkddlrwns@naver.com>
2025-05-11 22:50:05 +0900

    feat : client / protobuf 준비

---
+++ UnrealClient/Source/UnrealClient/UnrealClient.Build.cs
@@ -0,0 +1,28 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealClient : ModuleRules
+{
+	public UnrealClient(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(["Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"]);
+
+		PrivateDependencyModuleNames.AddRange(["ProtobufCore"]);
+
+		PrivateIncludePaths.AddRange([
+			"UnrealClient/",
+			"UnrealClient/Network"
+        ]);
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
